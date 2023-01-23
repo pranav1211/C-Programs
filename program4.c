f379eeeb7b7bdb2fe18b5b46449b2b2dc493e811
Pranav Veeraghanta <35630070+pranav1211@users.noreply.github.com>
2023-01-23 16:50:44 +0530

    Create program4.c
    a program to find the sum of all even numbers between any to numbers given by the user

---
+++ program4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,sum;
+    printf("enter 2 numbers M and N :\n");
+    printf("M:");
+    scanf("%d",&m);
+    printf("N:");
+    scanf("%d",&n);
+
+   while(m<=n)
+    {
+        if(m%2==0)
+        {
+            sum=sum+m;
+            
+        }m++;
+    }
+    printf("%d",sum);
+    
+    return 0;
+}
