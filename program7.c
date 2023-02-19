8d2efc35e4a9c8f2d3f46f7d9a861fa3379afffc
Pranav Veeraghanta <35630070+pranav1211@users.noreply.github.com>
2023-02-19 11:42:56 +0530

    Create program7.c
    using Arrays to store and search for a book with id

---
+++ program7.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main() {
+    int book[] = {123,153,189,199,206,254,298,356,399,400,5063,6548,10034};
+   
+    int mid,sta,end,num;
+    int found = -1;
+   
+    printf("enter id to be searched : ");
+    scanf("%d",&num);
+   
+    int a = (sizeof(book))/4;
+   
+    sta = 0;
+    end = a-1;
+   
+    while(sta<=end)
+    {
+        mid = (sta+end)/2;
+        if (book[mid]==num)
+        {
+            printf("the book of id = %d ,has been found at %d position",book[mid],mid+1);
+            found = 1;
+            break;
+        }
+        if(book[mid]>num)
+            end=mid-1;
+        else
+            sta=mid+1;
+    }
+   
+    if(found==-1)
+        {
+            printf(" the book of id %d has not been found",num);
+        }
+}
